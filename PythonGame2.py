0aa6c4c3cdaaa359eb56119a8470188aad48dd80
ahmad174 <54255606+ahmad174@users.noreply.github.com>
2019-12-18 11:10:42 +0200

    First Commit

---
+++ PythonGame2.py
@@ -0,0 +1,154 @@
+"""
+Questions Game
+
+Created By  Ahmad Taha
+
+"""
+
+
+print("Q1)  how many days in a year")
+print("A)  410 ")
+print("B)  365 ")
+print("C)  266 ")
+print("D)  281\n ")
+
+N= 4
+score = 0
+
+while(True):
+    Ans1 = (input("Insert The Correct Ansewer         ").lower())
+    
+    if(Ans1 == "a"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+    elif(Ans1 == "b"):
+        print("The Correct Choice is B")
+        score +=100
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+
+    elif(Ans1 == "c"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+    elif(Ans1 == "d"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+    
+    else:
+        print("Print The Correct Choice A OR B OR C OR D")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Q2)  how many bones does the human have")
+print("A)  276 bones at birth , 206 bones by adulthood ")
+print("B)  270 bones at birth , 206 bones by adulthood  ")
+print("C)  266 bones at birth , 266 bones by adulthood  ")
+print("D)  360 bones at birth , 266 bones by adulthood \n ")
+
+
+while(True):
+    Ans1 = (input("Insert The Correct Ansewer         ").lower())
+    
+    if(Ans1 == "a"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+    elif(Ans1 == "b"):
+        print("The Correct Choice is B")
+        score +=100
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+
+    elif(Ans1 == "c"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+    elif(Ans1 == "d"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+    
+    else:
+        print("Choices   A   B   C   D")
+
+
+
+
+
+
+
+
+
+
+
+print("Q3)  how many days in a year")
+print("A)  410 ")
+print("B)  365 ")
+print("C)  266 ")
+print("D)  281\n ")
+
+
+while(True):
+    Ans1 = (input("Insert The Correct Ansewer         ").lower())
+    
+    if(Ans1 == "a"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+    elif(Ans1 == "b"):
+        print("The Correct Choice is B")
+        score +=100
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+
+    elif(Ans1 == "c"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+
+    elif(Ans1 == "d"):
+        print("The Correct Choice is B")
+        N-=1
+        print("Life = " + str(N)  +"         Score = " + str(score)+"\n\n\n")
+        break
+    
+    else:
+        print("Choices   A   B   C   D")
+
+
+
+
+
+input("Insert Enter To Exit")
+
+        
